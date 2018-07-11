823d957db7f02ae02451da30f181316155b6bdb6
Georgi_Yordanov <aaster23@protonmail.com>
2018-07-11 10:23:58 +0300

    Add project files.

---
+++ Nums to Stars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums_to_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
